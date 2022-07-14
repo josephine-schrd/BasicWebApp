13b7fea627f11487f336f63017309a77afe9239e
Josephine Schroeder <josephine.schroeder@tum.de>
2022-07-14 11:16:57 +0200

    .

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -1,18 +1,36 @@
 package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
-//Hallo Fritz
+
+import java.util.ArrayList;
+
+//Hallo
 @Service
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Josephine";
+            return "Josephine";
+        } else if (query.contains(("number"))) {
+            String[] LIST = query.split(",");
+
+            int largestInt = 0;
+
+            for (String s : LIST
+            ) {
+                int a = Integer.parseInt(s);
+                if (largestInt <= a){
+                    largestInt = a;
+                }
+            }
+
+            return largestInt + "";
+
         } else { // TODO extend the programm here
             return "";
         }
